48659a3f170a48a48fc884f26f6a5740c03c9559
Seelpy <maks-veselov-2003@mail.ru>
2025-02-06 20:16:50 +0300

    1 add jump

--- model/animation.go
+++ model/animation.go
@@ -10,30 +10,40 @@ type Animation interface {
 
 func NewAnimation(figure Figure, speed float64, maxY float64, minY float64) Animation {
 	return &animation{
-		figure:   figure,
-		speed:    speed,
-		maxY:     maxY,
-		minY:     minY,
-		time:     0,
-		duration: 1,
+		figure:    figure,
+		speed:     speed,
+		maxY:      maxY,
+		minY:      minY,
+		time:      0,
+		duration:  1,
+		decayRate: 0.5,
 	}
 }
 
 type animation struct {
-	figure   Figure
-	speed    float64
-	maxY     float64
-	minY     float64
-	time     float64
-	duration float64 // Длительность одного полного прыжка (в секундах)
+	figure    Figure
+	speed     float64
+	maxY      float64
+	minY      float64
+	time      float64
+	decayRate float64
+	duration  float64 // Длительность одного полного прыжка (в секундах)
 }
 
 func (a *animation) Next() Figure {
 	t := math.Mod(a.time/a.duration, 1.0)
 
-	newY := a.minY + (a.maxY-a.minY)*(-4*(t-0.5)*(t-0.5)+1)
+	// Затухание основано на прошедшем времени
+	decay := math.Exp(-a.decayRate * a.time / a.duration) // decayRate определяет, насколько быстро затухает прыжок
+
+	// Вычисляем новую позицию Y, принимая во внимание затухание
+	newY := a.minY + (a.maxY-a.minY)*(-4*(t-0.5)*(t-0.5)+1)*decay
 
 	a.time += a.speed
+
+	if a.time > 5 {
+		a.time = 0
+	}
 
 	position := a.figure.Position()
 	position.Y = newY
